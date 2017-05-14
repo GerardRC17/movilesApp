ce510dfbeb499a0719c8a55a5da0b18f65d42ee2
BaezCrdrm <dev.baez_crdrm@outlook.com>
2017-05-14 15:12:26 -0500

    Issue 4 - Conexion y obtencion de datos

--- app/src/main/java/mx/unam/primera/com/logic/Service.java
+++ app/src/main/java/mx/unam/primera/com/logic/Service.java
@@ -3,6 +3,7 @@ package mx.unam.primera.com.logic;
 import android.util.Log;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -10,6 +11,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import mx.unam.primera.com.model.Event;
 
@@ -30,12 +35,23 @@ public class Service
         try
         {
             // Cambiar por dirección web
-            String strUrl = "http://192.168.1.64/MovilesWebService/scripts/service/req_events.php?evId=" +
-                    ev_id;
+            String strUrl =
+                    "http://192.168.1.64/MovilesWebService/scripts/service/req_events.php?evId="
+                    //"Dirección web"
+                    //+ ev_id;
+                    + "null";
             URL url = new URL(strUrl);
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-            response = connection.getResponseCode();
             result = new StringBuilder();
+            try
+            {
+                response = connection.getResponseCode();
+            }
+            catch (Exception e)
+            {
+                Log.e("Conexion", e.getMessage());
+                return null;
+            }
 
             if(response == HttpURLConnection.HTTP_OK)
             {
@@ -50,8 +66,8 @@ public class Service
         }
         catch (Exception ex)
         {
-            //Log.println(1, "", "Error en leer o conectar: ");
-            //Log.d("D", ex.getMessage().toString());
+            Log.d("D", ex.getMessage().toString());
+            return null;
         }
 
         return result.toString();
@@ -70,5 +86,33 @@ public class Service
             return false;
         }
         return false;
+    }
+
+    public List<Event> getEventsList(String strJson)
+    {
+        List<Event> events = new ArrayList<>();
+        try
+        {
+            JSONArray json = new JSONArray(strJson);
+            Event event;
+            for (int i = 0; i < json.length(); i++)
+            {
+                event = new Event();
+                JSONObject ob = json.getJSONObject(i);
+                event.setId(ob.getString("ev_id"));
+                event.setName(ob.getString("ev_name"));
+                String strDate = ob.getString("ev_sch");
+                strDate = strDate.replace("-", "/");
+                SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.US);
+                event.setDate(format.parse(strDate));
+                events.add(event);
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.d("Error al deserializar", "Algo salio mal :S -- ");
+            Log.e("Deserializar", ex.getMessage().toString());
+        }
+        return events;
     }
 }
