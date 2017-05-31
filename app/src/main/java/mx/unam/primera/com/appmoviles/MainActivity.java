b0adc602923064762b25e0a60ef6ba971362c645
GerardRC17 <fearofpuppet17@gmail.com>
2017-05-31 18:12:12 -0500

    boton back

--- app/src/main/java/mx/unam/primera/com/appmoviles/MainActivity.java
+++ app/src/main/java/mx/unam/primera/com/appmoviles/MainActivity.java
@@ -1,5 +1,6 @@
 package mx.unam.primera.com.appmoviles;
 
+import android.app.FragmentManager;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.media.MediaPlayer;
@@ -30,6 +31,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.R.attr.onClick;
 import mx.unam.primera.com.logic.*;
@@ -41,12 +44,14 @@ public class MainActivity extends AppCompatActivity
 {
 
     Fragment fragment;
+    List<Integer> history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        history = new ArrayList<Integer>();
 
         if(!isTablet())
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
@@ -92,6 +97,7 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         navigate(R.id.nav_all);
+
     }
 
     private boolean isTablet()
@@ -105,8 +111,61 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+        }
+        else
+        {
+
+            /*if(getFragmentManager().getBackStackEntryCount() > 2) {
+                getFragmentManager().popBackStack();
+                } else {
+                    super.onBackPressed();
+            }*/
+
+            RelativeLayout rl = (RelativeLayout) findViewById(R.id.rLayoutEventDetails);
+            if (rl.getVisibility() == View.VISIBLE)
+                rl.setVisibility(View.GONE);
+            /*else
+            {
+                try
+                {
+
+                    if (history.size() > 0) {
+                        int _type = history.get(history.size() - 1);
+                        int _id = 0;
+                        Log.d("Id", String.valueOf(_type));
+                        history.remove(history.size() - 1);
+                        switch (_type)
+                        {
+                            case -1:
+                                _id = R.id.nav_all;
+                                break;
+                            case 1:
+                                _id=R.id.nav_americano;
+                                break;
+                            case 2:
+                                _id=R.id.nav_soccer;
+                                break;
+                            case 3:
+                                _id=R.id.nav_bascket;
+                                break;
+                            case 4:
+                                _id=R.id.nav_basebaññ;
+                                break;
+                            case 5:
+                                _id=R.id.nav_conciertos;
+                                break;
+                            case 6:
+                                _id=R.id.nav_event;
+                                break;
+                        }
+                        navigate(_id);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.e("Navegación de historial", ex.getMessage());
+                }
+            }*/
         }
     }
 
@@ -138,58 +197,78 @@ public class MainActivity extends AppCompatActivity
         fragment = new AllEvents();
         Bundle args = new Bundle();
 
+        int newNav = 0;
+
         switch (id)
         {
             case R.id.nav_all:
                 args.putSerializable("EventType", -1);
+                newNav = -1;
                 break;
             case R.id.nav_americano:
                 args.putSerializable("EventType", 1);
+                newNav = 1;
                 break;
             case R.id.nav_soccer:
                 args.putSerializable("EventType", 2);
+                newNav = 2;
                 break;
 
             case R.id.nav_bascket:
                 args.putSerializable("EventType", 3);
+                newNav = 3;
                 break;
 
             case R.id.nav_basebaññ:
                 args.putSerializable("EventType", 4);
+                newNav = 4;
                 break;
 
             case R.id.nav_conciertos:
                 args.putSerializable("EventType", 5);
+                newNav = 5;
                 break;
 
             case R.id.nav_event:
                 args.putSerializable("EventType", 6);
+                newNav = 6;
                 break;
         }
         fragment.setArguments(args);
 
         navigate(R.id.rLayoutEventList, fragment);
+        history.add(newNav);
         return true;
     }
 
-    public void navigate(int replaced, Fragment frag)
-    {
-        RelativeLayout rl = (RelativeLayout)findViewById(R.id.rLayoutEventDetails);
-        if(replaced == R.id.rLayoutEventList)
+    public void navigate(int replaced, Fragment frag) {
+
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.rLayoutEventDetails);
+        if (replaced == R.id.rLayoutEventList)
             rl.setVisibility(View.GONE);
         else
             rl.setVisibility(View.VISIBLE);
 
-        getSupportFragmentManager().beginTransaction().replace(replaced,frag).commit();
+        /*getSupportFragmentManager().beginTransaction().addToBackStack(null).replace(replaced, frag)
+                .commit();*/
 
+        getSupportFragmentManager().beginTransaction().replace(replaced, frag)
+                .commit();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+
+        /*RelativeLayout rlD =(RelativeLayout)findViewById(R.id.rLayoutEventDetails);
+        rlD.setVisibility(View.GONE);*/
+
+
+
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
     }
+
 
 
 }
